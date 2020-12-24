883b709575da49b5c1a854a098bbc6ffa1fdeefc
YouheiKajiro <te0801068@gmail.com>
2020-12-24 20:40:49 +0900

    commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { TasksController } from './tasks/tasks.controller';
 import { TasksService } from './tasks/tasks.service';
 import { TasksModule } from './tasks/tasks.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(), TasksModule],
+  imports: [TypeOrmModule.forRoot(), TasksModule, UsersModule],
   controllers: [AppController, TasksController],
   providers: [AppService, TasksService],
 })
