282e1f2fea5c6b2941d74d5117b3721ff64994fe
YouheiKajiro <te0801068@gmail.com>
2020-12-24 20:34:27 +0900

    nest g service tasks

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { TasksController } from './tasks/tasks.controller';
+import { TasksService } from './tasks/tasks.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot()],
   controllers: [AppController, TasksController],
-  providers: [AppService],
+  providers: [AppService, TasksService],
 })
 export class AppModule {}
